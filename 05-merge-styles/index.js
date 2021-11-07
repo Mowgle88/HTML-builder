f5e966b5168570855a8b24d972ef507d23634461
Mowgle88 <mowgle1988@gmail.com>
2021-11-07 20:18:21 +0200

    feat: completed task 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,44 +1,61 @@
 // node 05-merge-styles
 
 const fs = require('fs');
-const { stat } = require('fs');
 const path = require('path');
 
 const stylesFolder = path.join(__dirname, 'styles');
 const projectFolder = path.join(__dirname, 'project-dist/bundle.css');
 
 
-
-
-fs.readdir(stylesFolder, {withFileTypes: true}, (err, files) => {
-
+async function writeFileToArray() {
   let array = [];
-  console.log(array)
 
-  // files = files.filter(file => file.isFile() && path.extname(file) === '.css');
-  for (const file of files) {
-
-    if(file.isFile() && path.extname(file.name) === '.css') {
-      // console.log(file);
-      let pathToFiles = path.join(stylesFolder, file.name);
-
-      fs.readFile(pathToFiles, 'utf-8', (error, data) => {
-
-        let arr = data;
-        array.push(arr)
-
-
+  await fs.readdir(stylesFolder, {withFileTypes: true}, (err, files) => {
   
-        // fs.writeFile(projectFolder, data, (error) => {
-        //   error ? console.log(error) : null;
-        // });
-      
-      });
-    }
-  }
-})
+    for (const file of files) {
+    
+      if(file.isFile() && path.extname(file.name) === '.css') {
+       
+        let pathToFiles = path.join(stylesFolder, file.name);
+          
+        fs.readFile(pathToFiles, 'utf-8', (error, data) => {
+          array.push(data)
+        });
+      }
+    } 
+  })
+  return array;
+}
 
-// console.log(array)
+async function bundleAssembly() {
+  try {
+    let arr = await writeFileToArray();
+
+    setTimeout(() => {
+      fs.writeFile(projectFolder, arr.join(''), (error) => {
+        error ? console.log(error) : console.log('Recording completed');
+      });
+    }, 2000)
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+bundleAssembly()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
